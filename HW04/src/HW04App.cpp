cda88607dd8f78235c47a87e13f7213b3b80a3e1
Mike Marsden <marsdems@muohio.edu>
2012-10-28 18:04:16 -0400

    Initial Commit

---
+++ HW04/src/HW04App.cpp
@@ -0,0 +1,34 @@
+#include "cinder/app/AppBasic.h"
+#include "cinder/gl/gl.h"
+
+using namespace ci;
+using namespace ci::app;
+using namespace std;
+
+class HW04App : public AppBasic {
+  public:
+	void setup();
+	void mouseDown( MouseEvent event );	
+	void update();
+	void draw();
+};
+
+void HW04App::setup()
+{
+}
+
+void HW04App::mouseDown( MouseEvent event )
+{
+}
+
+void HW04App::update()
+{
+}
+
+void HW04App::draw()
+{
+	// clear out the window with black
+	gl::clear( Color( 0, 0, 0 ) ); 
+}
+
+CINDER_APP_BASIC( HW04App, RendererGl )
